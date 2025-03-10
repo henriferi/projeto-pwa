dd8a3faf27ce937ac7156bd93dcbf07e3bbbc009
Henrique Fernandes <87838554+henriferi@users.noreply.github.com>
2025-03-10 20:23:25 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,12 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('meu-projeto-cache').then((cache) => {
       return cache.addAll([
-        '/projeto-pwa/', // Página inicial do seu app (precisa do caminho correto)
+        '/projeto-pwa/', 
         '/projeto-pwa/index.html',
         '/projeto-pwa/style.css',
         '/projeto-pwa/app.js',
         '/projeto-pwa/manifest.json',
-        '/projeto-pwa/icons/appstore.png', // Cuidado com a duplicação
-        '/projeto-pwa/icons/appstore.png',  // Remova o duplicado, se for o mesmo arquivo
+        '/projeto-pwa/icons/appstore.png', 
       ]);
     })
   );
