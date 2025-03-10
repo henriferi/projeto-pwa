13c02cf86b5be3e737d13231c82b3a75cf41ca57
Henrique Fernandes <87838554+henriferi@users.noreply.github.com>
2025-03-10 19:14:34 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    }).catch((error) => {
+      console.log('Erro ao registrar o Service Worker:', error);
+    });
+  }
